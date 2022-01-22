d1b6f9a43a551c4d5d4e16c5ec50d9d0a2327722
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-01-22 23:08:06 +0600

    Delete and Update functionality

--- Social_Media/Media_Functionalities/views.py
+++ Social_Media/Media_Functionalities/views.py
@@ -34,3 +34,18 @@ class PostListView(View):
         }
 
         return render(request, 'social/post_list.html', context)
+
+class PostDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm()
+
+        context = {
+            'post': post,
+            'form': form,
+        }
+
+        return render(request, 'social/post_detail.html', context)
+
+    def post(self, request, *args, **kwargs):
+        pass
